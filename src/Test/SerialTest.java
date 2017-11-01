e68150dcbc407225e3ae62dfed40d1a715de4ea8
s-tutida <s-tutida@ist.osaka-u.ac.jp>
2017-11-01 14:40:40 +0900

    SerialTest3

--- src/Test/SerialTest.java
+++ src/Test/SerialTest.java
@@ -1,54 +1,89 @@
 package Test;
 
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+ 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
+ 
 public class SerialTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// On the Raspberry Pi the Serial communication pins are connected to /dev/ttyAMA0
-	    CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier("/dev/ttyAMA0");
-	    if( portIdentifier.isCurrentlyOwned() ) {
-	        System.out.println( "Error: Port is currently in use" );
-	    } else {
-	        CommPort commPort = portIdentifier.open(this.getClass().getName(), 2000);
-
-	        if( commPort instanceof SerialPort) {
-	            SerialPort serialPort = ( SerialPort )commPort;
-	            serialPort.setSerialPortParams(
-	                    115200,
-	                    SerialPort.DATABITS_8,
-	                    SerialPort.STOPBITS_1,
-	                    SerialPort.PARITY_NONE );
-
-	            InputStream in = serialPort.getInputStream();
-	            OutputStream out = serialPort.getOutputStream();
-
-	            ( new Thread( new SerialReader( in ) ) ).start();
-	            ( new Thread( new SerialWriter( out ) ) ).start();
-	    	    		// Write imperial march as song '0':
-	    	    		write(out, 140, 0, 9, 57, 30, 57, 30, 57, 30, 53, 20, 60, 10, 57, 30, 53, 20, 60, 10, 57, 45);
-	    	    
-	    	    		// Play song '0':
-	    	    		write(out, 141, 0);
-	    	    		
-	        }
-	    }
-
-	}
-	    
-	    
-    private void write(OutputStream out, int... data) throws IOException {
-	    
-    		// Sigh, unsigned Java:
-	    byte[] output = new byte[data.length];
-	    
-	    for(int i = 0; i < data.length; i++) {
-	    		output[i] = (byte)(data[i]&0xFF);
-	    }
-	    out.write(output); 
+ 
+    void connect(String portName) throws Exception {
+        CommPortIdentifier portIdentifier = CommPortIdentifier
+                .getPortIdentifier(portName);
+        if (portIdentifier.isCurrentlyOwned()) {
+            System.out.println("Error: Port is currently in use");
+        } else {
+            int timeout = 2000;
+            CommPort commPort = portIdentifier.open(this.getClass().getName(),
+                    timeout);
+ 
+            if (commPort instanceof SerialPort) {
+                SerialPort serialPort = (SerialPort) commPort;
+                serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8,
+                        SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+ 
+                InputStream in = serialPort.getInputStream();
+                OutputStream out = serialPort.getOutputStream();
+ 
+                (new Thread(new SerialReader(in))).start();
+                (new Thread(new SerialWriter(out))).start();
+ 
+            } else {
+                System.out
+                        .println("Error: Only serial ports are handled by this example.");
+            }
+        }
     }
-
+ 
+    public static class SerialReader implements Runnable {
+ 
+        InputStream in;
+ 
+        public SerialReader(InputStream in) {
+            this.in = in;
+        }
+ 
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            try {
+                while ((len = this.in.read(buffer)) > -1) {
+                    System.out.print(new String(buffer, 0, len));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+ 
+    public static class SerialWriter implements Runnable {
+ 
+        OutputStream out;
+ 
+        public SerialWriter(OutputStream out) {
+            this.out = out;
+        }
+ 
+        public void run() {
+            try {
+                int c = 0;
+                while ((c = System.in.read()) > -1) {
+                    this.out.write(c);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+ 
+    public static void main(String[] args) {
+        try {
+            (new SerialTest()).connect("/dev/ttyUSB0");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
