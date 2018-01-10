ecfef782c142c02074e2ededf7cc0a9549298459
s-tutida <s-tutida@ist.osaka-u.ac.jp>
2018-01-10 12:29:53 +0900

    SerialTest

--- src/RoombaTest/RMonitor.java
+++ src/RoombaTest/RMonitor.java
@@ -1,10 +1,7 @@
 package RoombaTest;
 
 import Tsuchida.ComponentManager;
-import Tsuchida.Monitor;
-import org.opencv.core.Core;                                                                                            
-import org.opencv.core.CvType;                                                                                          
-import org.opencv.core.Mat;   
+import Tsuchida.Monitor;  
 
 public class RMonitor extends Monitor{
 
@@ -16,17 +13,6 @@ public class RMonitor extends Monitor{
 	@Override
 	public Object getData() {
 		
-		
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Mat im = Imgcodecs.imread("test.jpg");// 入力画像の取得
-		Mat hsv = new Mat();
-		Mat mask = new Mat();
-		Mat im2 = new Mat();
-		Imgproc.cvtColor(tempMat, tempMat, Imgproc.COLOR_RGBA2BGR);
-		Imgproc.cvtColor(tempMat, tempMat, Imgproc.COLOR_BGR2HSV);
-		Core.inRange(tempMat, new Scalar(0, 100, 30), new Scalar(5, 255, 255), tempMat);//指定した画素値の範囲内にある領域をマスク画像で取得する. ノイズ除去後の画像を保存する.
-		Imgproc.cvtColor(tempMat, tempMat, Imgproc.COLOR_GRAY2BGRA);
-	
 		return null;
 	}
 
---
+++ src/RoombaTest/SerialTest.java
@@ -0,0 +1,122 @@
+package RoombaTest;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+public class SerialTest {
+
+		InputStream in = null;
+		OutputStream out = null;
+
+		//シリアルポートとの接続を確立する関数
+	    void connect(String portName) throws Exception {
+	        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+	        if (portIdentifier.isCurrentlyOwned()) {
+	            System.out.println("Error: Port is currently in use");
+	        } else {
+	            int timeout = 2000;
+	            CommPort commPort = portIdentifier.open(this.getClass().getName(),timeout);
+
+	            if (commPort instanceof SerialPort) {
+	                SerialPort serialPort = (SerialPort) commPort;
+	                serialPort.setSerialPortParams(115200, SerialPort.DATABITS_8,SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+
+	                in = serialPort.getInputStream();
+	                out = serialPort.getOutputStream();
+
+	                (new Thread(new SerialReader(in))).start();//インプット用のスレッド
+	                (new Thread(new SerialWriter(out))).start();//アウトプット用のスレッド
+
+	            } else {
+	                System.out.println("Error: Only serial ports are handled by this example.");
+	            }
+	        }
+	    }
+
+	    //シリアルポートからのインプットを扱うスレッド
+	    public static class SerialReader implements Runnable {
+
+	        InputStream in;
+
+	        public SerialReader(InputStream in) {
+	            this.in = in;
+	        }
+
+	        public void run() {
+	            byte[] buffer = new byte[1024];
+	            int len = -1;
+	            try {
+	                while ((len = this.in.read(buffer)) > -1) {
+	                	    //TODO Read byteに変更する.
+	                	    int inputValue = Integer.parseInt(new String(buffer));
+		            	    System.out.println("DEBUG : SerialReader Len" + len);
+		            	    System.out.println("DEBUG : SerialReader InputValue" + (byte)(inputValue&0xFF));
+	                }
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    //シリアルポートからのアウトプットを扱うスレッド
+	    public static class SerialWriter implements Runnable {
+
+	        OutputStream out;
+
+	        public SerialWriter(OutputStream out) {
+	            this.out = out;
+	        }
+
+	        //TODO 入力により, 用意する
+	        public void run() {
+	            try {
+
+	                String line = null;
+	     			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));//標準入力から受け付ける
+	    			while ((line = br.readLine()) != null){
+
+	                	    //debug
+	                	    System.out.println("DEBUG : SerialWriter " + line);
+	                	    int inputValue = Integer.parseInt(new String(line));
+	                	    System.out.println("DEBUG : SerialWriter Byte " + (byte)(inputValue&0xFF));
+
+//	                	    if(c==1) {
+	                	    		// Write imperial march as song '0':
+//	                	    		write(out, 140, 0, 9, 57, 30, 57, 30, 57, 30, 53, 20, 60, 10, 57, 30, 53, 20, 60, 10, 57, 45);
+//	                    		write(out, 141, 0);
+//	                	    }
+//	                	    out.write((byte)(inputValue&0xFF));
+	                }
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+
+	    private static void write(OutputStream out, int... data) throws IOException {
+	        // Sigh, unsigned Java:
+	        byte[] output = new byte[data.length];
+	        for(int i = 0; i < data.length; i++) {
+	            output[i] = (byte)(data[i]&0xFF);
+	        }
+	        out.write(output);
+	    }
+
+		public static void main(String arg[]) throws Exception{
+			    SerialTest rs = new SerialTest();
+	            rs.connect("/dev/ttyUSB0");
+		}
+
+		public SerialTest(){
+
+		}
+
+	}
