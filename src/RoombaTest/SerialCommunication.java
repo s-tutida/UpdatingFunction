6cdf40bfa0bef837594861ca5a929e97c45036f5
s-tutida <s-tutida@ist.osaka-u.ac.jp>
2018-01-11 04:31:54 +0900

    微調整

--- src/RoombaTest/SerialCommunication.java
+++ src/RoombaTest/SerialCommunication.java
@@ -53,17 +53,17 @@ public class SerialCommunication {
                 	    		     break;
                 	    	case 5 : clean(out);
                 	    	         break;
-                	    	case 61 : drive(out, -30, 0);//forward_l
+                	    	case 61 : drive(out, -20, 0);//forward_l
                 	    	         break;
-                	    	case 62 : drive(out, -60, 0);//forward_h
+                	    	case 62 : drive(out, -70, 0);//forward_h
        	    	         		 break;
                 	    	case 71: driveDirect(out, -10, 10);//clockwise_l
     	         		 		break;
-                   	case 72: driveDirect(out, -30, 30);//clockwise_h
+                   	case 72: driveDirect(out, -20, 20);//clockwise_h
 	         		 		break;
                    	case 81: driveDirect(out, 10, -10);//counter-clockwise_l
                    			break;
-                   	case 82: driveDirect(out, 30, -30);//counter-clockwise_h
+                   	case 82: driveDirect(out, 20, -20);//counter-clockwise_h
                    			break;	         		 		
                 	    	case 9 : break;//sleep
                 	    	default : break;
