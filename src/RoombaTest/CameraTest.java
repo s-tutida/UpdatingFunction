3e70ec42ae3b5223cd9d5bb51867979644fa66b8
s-tutida <s-tutida@ist.osaka-u.ac.jp>
2018-01-17 17:47:07 +0900

    テンプレート抽出のサンプルコード

--- src/RoombaTest/CameraTest.java
+++ src/RoombaTest/CameraTest.java
@@ -3,20 +3,30 @@ package RoombaTest;
 public class CameraTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	Mat tmpl = Highgui.imread(tmplFile.getAbsolutePath(), Highgui.CV_LOAD_IMAGE_COLOR);
+		
+		USBcamera uc = new USBcamera();
+		uc.run();
+		((VideoCapture)o).read(webcam_image);
+		if( !(webcam_image.empty()) ) {
+		//用意
+		Mat webcam_image=new Mat();// 入力画像の取得
+		Mat tmp = Imgcodecs.imread("tmp.jpg");//テンプレートの用意	
 	
-	Mat result = new Mat(img.rows() - tmpl.rows() + 1, img.cols() - tmpl.cols() + 1, CvType.CV_32FC1);
-	Imgproc.threshold(result, result, 0.8, 1.0, Imgproc.THRESH_TOZERO);
-	
-	for (int i=0;i<result.rows();i++) {
-		  for (int j=0;j<result.cols();j++) {
-		    if (result.get(i, j)[0] > 0) {
-		      Core.rectangle(img, new Point(j, i), new Point(j + tmpl.cols(), i + tmpl.rows()), new Scalar(0, 0, 255));
-		    }
-		  }
+		Mat result = new Mat();
+
+		Imgproc.matchTemplate(im, tmp, result, Imgproc.TM_CCOEFF_NORMED);	//テンプレートマッチング
+		Imgproc.threshold(result, result, 0.8, 1.0, Imgproc.THRESH_TOZERO); 	// 検出結果から相関係数がしきい値以下の部分を削除
+		
+		// テンプレート画像の部分を元画像に赤色の矩形で囲む
+		for (int i=0;i<result.rows();i++) {
+			for (int j=0;j<result.cols();j++) {
+				if (result.get(i, j)[0] > 0) {
+					Imgproc.rectangle(im, new Point(j, i), new Point(j + tmp.cols(), i + tmp.rows()), new Scalar(0, 0, 255));
+					System.out.println(i + " "+j);
+				}
+			}
+		}
+
+		Imgcodecs.imwrite("result.jpg", im);// 画像の出力
 	}
 }
