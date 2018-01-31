2c026a8906ed77c85e4321427bd6eeb9a907d521
s-tutida <s-tutida@ist.osaka-u.ac.jp>
2018-02-01 05:16:15 +0900

    d

--- src/RoombaTest/RPlan.java
+++ src/RoombaTest/RPlan.java
@@ -17,19 +17,19 @@ public class RPlan extends Plan{
 	    System.out.println("DEBUG : mode in RPlan instance is " + mode);
 		
 		if(mode == -1) {
-			String commands= "1,0,4,9,5";//Start cleaning
+			String commands= "1,0,4,8,5";//Start cleaning
 			return commands;
 		}else if(mode == 1) {
-			String commands= "1,4,81,62";
+			String commands= "1,4,74,6";
 		    return commands;
 		}else if(mode == 2){
-			String commands= "1,4,71,62";
+			String commands= "1,4,71,6";
 			return commands;
 		}else if(mode == 3) {
-			String commands= "1,4,72,61";
+			String commands= "1,4,72,6";
 			return commands;
 		}else if(mode == 4) {
-			String commands= "1,4,82,61";
+			String commands= "1,4,75,6";
 			return commands;
 		}else{
             String commands= "8";
