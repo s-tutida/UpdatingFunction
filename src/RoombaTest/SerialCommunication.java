0cd574d94e673aba11236430f2cb96d9886fb3ec
Shinya Tsuchida <u085195a@ecs.osaka-u.ac.jp>
2018-10-21 08:01:58 +0000

    modify or delete directory on the Raspberry Pi

--- src/RoombaTest/SerialCommunication.java
+++ src/RoombaTest/SerialCommunication.java
@@ -41,27 +41,27 @@ public class SerialCommunication {
             	    
             	    switch(inputValue) {
             	    		case 0: write(out, 140, 0, 9, 57, 30, 57, 30, 57, 30, 53, 20, 60, 10, 57, 30, 53, 20, 60, 10, 57, 45);
-            	    				write(out, 141, 0);
+					write(out, 141, 0);
                 	    	        break;
                 	    	case 1: startup(out);
                 	    	        break;
                 	    	case 2:	stop(out);
-                	    			 break;
+                	    		 break;
                 	    	case 3 : safeMode(out);
-                	    			 break;
+                	    		 break;
                 	    	case 4 : fullMode(out);
-                	    		     break;
+                	    		 break;
                 	    	case 5 : clean(out);
                 	    	         break;
                 	    	case 6 : drive(out, -50, 0);//forward_l
                 	    	         break;
                 	    	case 71: driveDirect(out, -10, 10);//clockwise_l
-    	         		 		break;
-                   	case 72: driveDirect(out, -15, 15);//clockwise_h
-	         		 		break;
-                   	case 81: driveDirect(out, 10, -10);//counter-clockwise_h
+    	         		 	break;
+                   		case 72: driveDirect(out, -15, 15);//clockwise_h
+	         		 	break;
+                   		case 81: driveDirect(out, 10, -10);//counter-clockwise_h
                    			break;
-                   	case 82:driveDirect(out, 15, -15);//counter-clockwise_h
+                   		case 82:driveDirect(out, 15, -15);//counter-clockwise_h
                    		    break;
                 	    	case 8 : break;//sleep
                 	    	default : break;
@@ -123,6 +123,9 @@ public class SerialCommunication {
 	    public static void clean(OutputStream out) throws IOException {
 //	        System.out.println("Sending 'clean' command to roomba.");
 	        write(out, OPC_CLEAN);
+		int i=1;
+		while(i==1){
+		}					
 	    }
 
 
