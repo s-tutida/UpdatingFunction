e171fefe46e54f5c6f9809f790c22229a88d1e84
Shinya Tsuchida <u085195a@ecs.osaka-u.ac.jp>
2018-10-21 11:20:54 +0000

    Web Application example

---
+++ src/WebApplicationTest/TestAnalysis.java
@@ -0,0 +1,28 @@
+package WebApplicationTest;
+
+import Tsuchida.Analyze;
+import Tsuchida.ComponentManager;
+import Tsuchida.Configuration;
+
+public class TestAnalysis extends Analyze {
+	
+	private int referenceValue = 0;
+
+	public TestAnalysis(ComponentManager cm, String name, Configuration conf) {
+		super(cm, name, conf);
+		referenceValue =  Integer.parseInt(conf.getValue(name).trim());
+	}
+
+	@Override
+	public Object analysis(Object o) {
+		if(Integer.parseInt( (String) o ) > referenceValue) {
+			return 1;
+		}else {
+			return 0;
+		}
+		
+	}
+}
+
+
+
