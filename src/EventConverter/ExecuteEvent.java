b4a2f0665be05d17acd31d7f3d3df330dec12cce
s-tutida <u085195a@ecs.osaka-u.ac.jp>
2018-10-24 04:52:58 +0900

    debug

--- src/EventConverter/ExecuteEvent.java
+++ src/EventConverter/ExecuteEvent.java
@@ -68,11 +68,14 @@ public class ExecuteEvent extends Execute{
 	    			}else if(new_event.equals("Spot")) {
 	    				System.out.println("Spot command");
 	    				this.sc.send_command_original(5);
-	    				this.sc.send_command(5);
 	    			}
-	    			System.out.println("event equal Clean:" + new_event.equals("Clean"));
-	    			System.out.println("event equal Spot:" + new_event.equals("Spot"));
 	    		}
+	    		
+			try {
+				Thread.sleep(100000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 
 	        break;
         case 3:
