1b7a3c35d1259dc0ce7d984c86e91408f2f63264
s-tutida <u085195a@ecs.osaka-u.ac.jp>
2018-10-24 05:52:23 +0900

    ちょ末い

--- src/EventConverter/ExecuteEvent.java
+++ src/EventConverter/ExecuteEvent.java
@@ -59,8 +59,17 @@ public class ExecuteEvent extends Execute{
         		// eventの送信
 //            this.sc.send_command_original(2);
 			this.sc.send_command_original(1);//start up 
+			try {
+				Thread.sleep(700);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			this.sc.send_command_original(3);//full mode
-			
+			try {
+				Thread.sleep(700);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 	    		while(event_list.peekLast()!=null) {
 	    			String new_event = event_list.pollLast();
 	    			System.out.println("      MAPE-K loop will send this event : "+ new_event);
