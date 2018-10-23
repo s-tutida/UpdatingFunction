fa5904f51408e83bfaf4a6461de69155d3647b8d
s-tutida <u085195a@ecs.osaka-u.ac.jp>
2018-10-23 13:50:54 +0900

    ログの種類変更

--- src/RoombaTest/RExecute.java
+++ src/RoombaTest/RExecute.java
@@ -37,8 +37,8 @@ public class RExecute extends Execute{
 					//MAPEの終了
 					if(Integer.parseInt(command)==5) {//start to clean
 						check = 1;
-						System.out.println("      Arrive at target point");
-						System.out.println("      Send arriveSpot event to EventConverter.");
+						System.out.println("***  Arrive at target point  ***  ");
+						System.out.println("***  Send arriveSpot event to EventConverter.  ***  ");
 						sc.send_command(2);//Reset
 						try {
 							this.cm.exit();
