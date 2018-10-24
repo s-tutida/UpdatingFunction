a12a7114e6653d734c9c8e1007212ce91623e96f
s-tutida <u085195a@ecs.osaka-u.ac.jp>
2018-10-24 10:06:14 +0900

    前へは移動しないように変更.

--- src/RoombaTest/SerialCommunication.java
+++ src/RoombaTest/SerialCommunication.java
@@ -160,8 +160,10 @@ public class SerialCommunication {
                 	    		 break;
                 	    	case 5 : clean_spot(out);//Spot
                 	    	         break;
-                	    	case 6 : drive(out, -100, 0);//forward_l
-                	    	         break;
+                	    	case 6 : 
+                	    		System.out.println("Dont move to forward");;
+//                	    		drive(out, -100, 0);//forward_l
+                	    	    break;
                 	    	case 71: 
                 	    		System.out.println("-20, 20");
                 	    		driveDirect(out, -20, 20);//clockwise_l
