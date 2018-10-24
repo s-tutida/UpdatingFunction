c05b79817ed4cd928108e3db77eb56c97974b9a7
s-tutida <u085195a@ecs.osaka-u.ac.jp>
2018-10-24 11:05:49 +0900

    調整

--- src/RoombaTest/ObjectDetector.java
+++ src/RoombaTest/ObjectDetector.java
@@ -55,7 +55,7 @@ public class ObjectDetector {
 		
 		int sum = count1 + count2 + count3 + count4;
 		System.out.println(sum);
-		if(sum > 560) {
+		if(sum > 2500) {
 			answer = -1;//終了
 		}
 		return String.valueOf(answer);
