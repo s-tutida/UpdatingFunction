a1601d8fe8bbe8a6cca9cb4ea123ede75896b6fb
s-tutida <u085195a@ecs.osaka-u.ac.jp>
2018-10-29 12:44:10 +0900

    確認

--- src/EventConverter/MonitorEvent.java
+++ src/EventConverter/MonitorEvent.java
@@ -22,8 +22,8 @@ public class MonitorEvent extends Monitor{
 		
 		//internal eventがない場合, ボタン押待ち状態でボタンが押されるのを待つ
 		sc.resetButtonEvent();
+		this.sc.send_command_original(0);//受信モード
 		while(true) {
-			this.sc.send_command_original(0);//受信モード
 			int button_event = -1;
 			if((button_event = sc.getButtonEvent()) != -1) {
 	        		//Clean, Spot, EndSpotの3つのみ.
